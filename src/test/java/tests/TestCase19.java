beb7259460850134a2bd4775014b3a89d61c18d6
AnkitKathale <kathaleankit4@gmail.com>
2025-07-15 00:32:47 +0530

    19-26 test cases

--- src/test/java/tests/TestCase19.java
+++ src/test/java/tests/TestCase19.java
@@ -3,6 +3,9 @@ import io.qameta.allure.*;
 import org.json.simple.parser.ParseException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.ProductsPage;
+import utils.JSONReader;
 
 import java.io.IOException;
 import java.util.List;
@@ -23,18 +26,45 @@ public class TestCase19 extends TestBasic{
             6. Verify that user is navigated to brand page and brand products are displayed
             7. On left side bar, click on any other brand link
             8. Verify that user is navigated to that brand page and can see products""")
-    public void viewCartBrandProducts() throws IOException, ParseException {
+    public void viewCartBrandProducts() throws IOException, ParseException, java.text.ParseException {
         verifyThatBrandsAreVisibleOnLeftSideBar();
         verifyThatUserIsNavigatedToBrandPageAndBrandProductsAreDisplayed();
         verifyThatUserIsNavigatedToThatBrandPageAndCanSeeProducts();
     }
     @Step("Verify that Brands are visible on left side bar")
-    private void verifyThatUserIsNavigatedToThatBrandPageAndCanSeeProducts() {
-    }
-    @Step("Verify that user is navigated to brand page and brand products are displayed")
-    private void verifyThatUserIsNavigatedToBrandPageAndBrandProductsAreDisplayed() {
-    }
-    @Step("Verify that user is navigated to that brand page and can see products")
     private void verifyThatBrandsAreVisibleOnLeftSideBar() {
+        boolean brandsIsDisplayed = new HomePage(getDriver())
+                .productsButtonClick()
+                .getBrands()
+                .isDisplayed();
+        Assert.assertTrue(brandsIsDisplayed, "Verify that Brands are visible on left side bar");
+    }
+
+    @Step("Verify that user is navigated to brand page and brand products are displayed")
+    private void verifyThatUserIsNavigatedToBrandPageAndBrandProductsAreDisplayed() throws IOException, ParseException, java.text.ParseException {
+        String titleTextCenter = new ProductsPage(getDriver())
+                .poloBrandClick()
+                .getTitleTextCenter()
+                .getText();
+        Assert.assertEquals(titleTextCenter, "BRAND - POLO PRODUCTS", "Verify that user is navigated to brand page");
+
+        List<String> productsNames = new ProductsPage(getDriver()).getProductsSearchNames();
+        for (int i = 0; i < productsNames.size(); i++) {
+            Assert.assertEquals(productsNames.get(i), JSONReader.poloBrandProducts(String.valueOf(i)), "Verify that brand products are displayed");
+        }
+    }
+
+    @Step("Verify that user is navigated to that brand page and can see products")
+    private void verifyThatUserIsNavigatedToThatBrandPageAndCanSeeProducts() throws IOException, ParseException, java.text.ParseException {
+        String titleTextCenter = new ProductsPage(getDriver())
+                .madameBrandClick()
+                .getTitleTextCenter()
+                .getText();
+        Assert.assertEquals(titleTextCenter, "BRAND - MADAME PRODUCTS", "Verify that user is navigated to that brand page");
+
+        List<String> productsNames = new ProductsPage(getDriver()).getProductsSearchNames();
+        for (int i = 0; i < productsNames.size(); i++) {
+            Assert.assertEquals(productsNames.get(i), JSONReader.madameBrandProducts(String.valueOf(i)), "Verify that can see products");
+        }
     }
 }
